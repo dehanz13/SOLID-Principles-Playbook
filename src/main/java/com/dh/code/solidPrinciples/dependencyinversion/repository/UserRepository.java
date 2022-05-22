b49b5f36a6f3c5d6ea7d3e1ae59053c942a18089
dehanz13 <hansel.danniel@gmail.com>
2022-05-22 04:53:58 -0500

    add: dependency inversion with constant parameters

--- src/main/java/com/dh/code/solidPrinciples/dependencyinversion/repository/UserRepository.java
+++ src/main/java/com/dh/code/solidPrinciples/dependencyinversion/repository/UserRepository.java
@@ -9,15 +9,25 @@ import java.util.Optional;
 
 public class UserRepository {
 
-    private Map<Integer, User> usersMap = Collections.unmodifiableMap(new HashMap<Integer, User>() {
-        {
-            put(1, new User(1, "Batman"));
-            put(1, new User(2, "Robbin"));
-        }
-    });
+    /* DI */
+//    private Map<Integer, User> usersMap = Collections.unmodifiableMap(new HashMap<Integer, User>() {
+//        {
+//            put(1, new User(1, "Batman"));
+//            put(1, new User(2, "Robbin"));
+//        }
+//    });
+//    public Optional<User> search(Integer userId) {
+//        return Optional.ofNullable(usersMap.get(userId));
+//    }
+
+    /* DI with Contant Parameters */
+    private Map<Integer, User> usersMap;
+
+    public UserRepository(Map<Integer, User> users) {
+        this.usersMap = users;
+    }
 
     public Optional<User> search(Integer userId) {
         return Optional.ofNullable(usersMap.get(userId));
     }
-
 }
